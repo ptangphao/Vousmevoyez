d9703965499a49fff3d3179ffd47b6964b816ed7
Patrick Tangphao <patrick.tangphao@gmail.com>
2017-11-04 01:42:26 -0700

    add company_url to deals model

--- db/migrate/20171104083121_create_deals.rb
+++ db/migrate/20171104083121_create_deals.rb
@@ -2,6 +2,7 @@ class CreateDeals < ActiveRecord::Migration
   def change
     create_table :deals do |t|
       t.string :company
+      t.string :company_url
       t.text :detail
 
       t.timestamps null: false
