9f5b69f3107e6eaf2ad769e63f8660718554738c
Patrick Tangphao <patrick.tangphao@gmail.com>
2017-11-04 02:00:04 -0700

    update DealsController to handle lack of deals

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,6 +1,6 @@
 class DealsController < ApplicationController
   def index
     @deals = Deal.all
-    @last_updated = @deals.last.created_at.in_time_zone("Pacific Time (US & Canada)") 
+    @last_updated = @deals.empty? ? Time.now : @deals.last.created_at.in_time_zone("Pacific Time (US & Canada)")
   end
 end
