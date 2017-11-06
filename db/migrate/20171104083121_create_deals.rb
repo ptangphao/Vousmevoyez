3f4bf09eedf1eb8db71bbc4584022f7d929364c7
Patrick Tangphao <patrick.tangphao@gmail.com>
2017-11-06 00:01:56 -0800

    add post_date attriute to Deals model

--- db/migrate/20171104083121_create_deals.rb
+++ db/migrate/20171104083121_create_deals.rb
@@ -3,6 +3,7 @@ class CreateDeals < ActiveRecord::Migration
     create_table :deals do |t|
       t.string :company
       t.string :company_url
+      t.string :post_date
       t.text :detail
 
       t.timestamps null: false
